058ed121b81f63233f473095764982bedc493aba
Christopher Makombe <125880140+christopher-codespaces@users.noreply.github.com>
2023-04-17 15:21:49 +0000

    The changes I made  updates the content of every status element by choosing all the status elements using the querySelectorAll() method. It then loops through every status element and updates the status message, depending on whether the order has been delivered or not. Additionally, it updates the count of each item in the order based on whether it has been delivered or not. These counts are obtained from the outstanding and delivered objects

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-const items = ['biscuits', 'donuts', 'pancakes'];
+//ROOt 1  Which is  Order 1
 
-let outstanding = {
-  biscuits: 0,
-  donuts: 0,
-  pancakes: 0
-};
-
-let delivered = {
-  biscuits: 0,
-  donuts: 0,
-  pancakes: 0
-};
-
-const orders = document.querySelectorAll('dl');
-
-for (let i = 0; i < orders.length; i++) {
-  const order = orders[i];
-  const isDelivered = order.getAttribute('data-delivered') === 'true';
-
-  items.forEach(item => {
-    const count = parseInt(order.getAttribute(`data-${item}`));
-    if (isDelivered) {
-      delivered[item] += count;
-    } else {
-      outstanding[item] += count;
-    }
-  });
+const root1={
+    biscuits: document.querySelector('[data-biscuits="10" ]'),
+    donuts: document.querySelector('[data-donuts="13" ]'),
+    pancakes: document.querySelector('[data-pancakes="0"]'),
+    status: document.querySelector('[data-delivered="false"]'),
 }
 
-console.log('Outstanding:', outstanding);
-console.log('Delivered:', delivered);
+   document.querySelector('dl[data-key="order1"] div.biscuits dd.count').innerHTML=root1.biscuits.dataset.biscuits
+   document.querySelector('dl[data-key="order1"] div.donuts dd.count').innerHTML=root1.donuts.dataset.donuts 
+   document.querySelector('dl[data-key="order1"] div.pancakes dd.count').innerHTML=root1.pancakes.dataset.pancakes
+   document.querySelector('dl[data-key="order1"] div.status dd').innerHTML=root1.status.dataset.status==='false'? 'delivered': 'pending'
+
+   
+
+// ROOT 2 Which is order 2
+
+   const root2={
+    biscuits: document.querySelector('[data-biscuits="5" ]'),
+    donuts: document.querySelector('[data-donuts="0" ]'),
+    pancakes: document.querySelector('[data-pancakes="2"]'),
+    status: document.querySelector('[data-delivered="false"]'),
+}
+
+   document.querySelector('dl[data-key="order2"] div.biscuits dd.count').innerHTML=root2.biscuits.dataset.biscuits
+   document.querySelector('dl[data-key="order2"] div.donuts dd.count').innerHTML=root2.donuts.dataset.donuts 
+   document.querySelector('dl[data-key="order2"] div.pancakes dd.count').innerHTML=root2.pancakes.dataset.pancakes
+   document.querySelector('dl[data-key="order2"] div.status dd').innerHTML=root2.status.dataset.status==='false'? 'delivered': 'pending'
+
+  //ROOT 3 Which is order 3
+   const root3={
+    biscuits: document.querySelector('[data-biscuits="12" ]'),
+    donuts: document.querySelector('[data-donuts="11" ]'),
+    pancakes: document.querySelector('[data-pancakes="15"]'),
+    status: document.querySelector('[data-delivered="true"]'),
+}
+
+   document.querySelector('dl[data-key="order3"] div.biscuits dd.count').innerHTML=root3.biscuits.dataset.biscuits
+   document.querySelector('dl[data-key="order3"] div.donuts dd.count').innerHTML=root3.donuts.dataset.donuts 
+   document.querySelector('dl[data-key="order3"] div.pancakes dd.count').innerHTML=root3.pancakes.dataset.pancakes
+   document.querySelector('dl[data-key="order3"] div.status dd').innerHTML=root3.status.dataset.status==='True'? 'pending': 'delivered'//tenary
+
+   
 
 // This code is designed to process a list of orders for three different types of items: biscuits, donuts, and pancakes. To keep track of the orders, two objects are initialized at the beginning of the code: outstanding and delivered. These objects will store the number of each item that has been delivered and that is still outstanding. The keys in each object correspond to the item types and the values are initialized to zero.
 
